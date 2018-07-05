faee688638342839b5e64bbd2055e1f806abef84
Марина <@DESKTOP-1C4J9CR>
2018-07-05 22:31:16 +0600

    Add project files.

---
+++ HT3_2606/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT3_2606
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
