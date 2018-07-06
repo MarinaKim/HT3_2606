de7732c2351b7b1183aaea6411458a0f7a4d3fe1
Марина <marikaa@mail.ru>
2018-07-06 14:39:07 +0600

    -

--- HT3_2606/Program.cs
+++ HT3_2606/Program.cs
@@ -53,8 +53,65 @@ namespace HT3_2606
             Console.WriteLine();
             Console.WriteLine("----------------------------------------------------------");
 
+            int[] newArr = new int[arr.Length - 1];
+            for (int i = 0; i < index2; i++)
+            {
+                newArr[i] = arr[i];
+            }
 
-          
+            for (int i = index2; i < newArr.Length; i++)
+            {
+                newArr[i] = arr[i + 1];
+            }
+            arr = newArr;
+            foreach (int item in arr)
+                Console.Write("{0}\t", item);
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------");
+
+            Console.WriteLine("введите число, которое хотите удалить: ");
+            int k = Int32.Parse(Console.ReadLine());
+            for(int i = 0; i < arr.Length; i++)
+            {
+               if (arr[i] == k)
+                {
+                    int[] tmp = new int[arr.Length - 1];
+                    for (int j=0;j<i;j++)
+                    {
+                        tmp[j] = arr[j];
+                    }
+                    for(int j=i;j<tmp.Length;j++)
+                    {
+                        tmp[j] = arr[j + 1];
+                    }
+                    arr = tmp;
+                }
+            }
+            foreach (int item in arr)
+                Console.Write("{0}\t", item);
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------");
+
+
+            Console.Write("Введите индекс, куда хотите вставить элемент: ");
+            index2 = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите элемент: ");
+            int el = Int32.Parse(Console.ReadLine());
+            int[] tmp1 = new int[arr.Length + 1];
+            for (int i = 0; i < index2; i++)
+            {
+                tmp1[i] = arr[i];
+            }
+            tmp1[index2] = el;
+            for(int i = index2 + 1; i < tmp1.Length; i++)
+            {
+                tmp1[i] = arr[i-1];
+            }
+            
+            foreach (int item in tmp1)
+                Console.Write("{0}\t", item);
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------");
         }
     }
 }
